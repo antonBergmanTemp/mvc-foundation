905d18cc51b65ac66bee32a8d6f36c84ff36f035
antonBergmanTemp <95655983+antonBergmanTemp@users.noreply.github.com>
2021-12-07 00:12:21 +0200

    init

---
+++ Controllers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace MVC
+{
+    public class Controllers
+    {
+        private static Dictionary<Type, object> _controllers = new Dictionary<Type, object>();
+
+        //need to rework
+        public static T Create<T>(object model)
+        {
+            if (_controllers.ContainsKey(typeof(T)))
+                throw new InvalidOperationException($"{nameof(T)} alredy created");
+
+            T instance = (T)Activator.CreateInstance(typeof(T), new object[] { model });
+            _controllers.Add(typeof(T), instance);
+            return instance;
+        }   
+
+        public static object Get(Type type)
+        {
+            return _controllers[type];
+        }
+    }
+
+    public class MVCEventAttribute : Attribute { }
+}
---
+++ View.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Dynamic;
+using UnityEngine;
+
+namespace MVC
+{
+    //Hint for unity inspector
+    public abstract class WalletView : Initable<Wallet> { }
+}
